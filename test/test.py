59419897d28b351bcba3afbb29ad81733343f284
Andronik Ordian <andronik.ordian@gmail.com>
2015-05-12 23:58:50 +0300

    Test: make buffer page aligned. unlink tmp file

--- test/test.py
+++ test/test.py
@@ -2,6 +2,7 @@
 # coding=utf-8
 
 import os
+import resource
 import unittest
 
 try:
@@ -13,34 +14,39 @@ except ImportError:
       take a look at directio/README""")
 
 
+alignment = resource.getpagesize()
+
+
 class TestDirectio(unittest.TestCase):
 
     def setUp(self):
         super(TestDirectio, self).setUp()
         flags = os.O_RDWR | os.O_DIRECT | os.O_SYNC | os.O_CREAT | os.O_TRUNC
-        self.file = os.open('test.txt', flags, 0o666)
-        self.buffer = bytearray(512)
+        self.filename = 'test.txt'
+        self.fd = os.open(self.filename, flags, 0o666)
+        self.buffer = bytearray(alignment)
         self.msg = b'It just works!'
         self.buffer[:len(self.msg)] = self.msg
 
     def tearDown(self):
         super(TestDirectio, self).tearDown()
-        os.close(self.file)
-
+        os.close(self.fd)
+        os.unlink(self.filename)
+        
     def test_read_after_write(self):
         # can write only immutable buffer, so we wrap buffer in bytes
-        written = directio.write(self.file, bytes(self.buffer))
+        written = directio.write(self.fd, bytes(self.buffer))
         self.assertEqual(written, len(self.buffer))
-        os.lseek(self.file, 0, os.SEEK_SET)
-        got = directio.read(self.file, len(self.buffer))
+        os.lseek(self.fd, 0, os.SEEK_SET)
+        got = directio.read(self.fd, len(self.buffer))
         self.assertEqual(got, self.buffer)
 
     def test_fails_to_write_not_multiple_of_512(self):
-        self.assertRaises(ValueError, directio.write, self.file, self.msg)
+        self.assertRaises(ValueError, directio.write, self.fd, self.msg)
 
     def test_fails_to_read_not_multiple_of_512(self):
-        os.lseek(self.file, 0, os.SEEK_SET)
-        self.assertRaises(ValueError, directio.read, self.file, 511)
+        os.lseek(self.fd, 0, os.SEEK_SET)
+        self.assertRaises(ValueError, directio.read, self.fd, 511)
 
 
 if __name__ == '__main__':
